e2504b11c20b6d142b77d76b7a149b63ae2485b1
KornelijaSukyte <kornelija.suk@gmail.com>
2020-03-08 15:41:59 +0000

    add 3d segmentation

--- segment.m
+++ segment.m
@@ -1,4 +1,5 @@
-cloud = pcread('1/1_1.pcd');
+function new = segment(filename)
+cloud = pcread(filename);
 pcshow(cloud)
 
 % Filter for NaN and inf values
@@ -6,30 +7,27 @@ pcshow(cloud)
 loc_pc = cloud.Location;
 color_pc = im2double(cloud.Color);
 
-color_pc(:,1) = im2double(color_pc(:,1));
-color_pc(:,2) = im2double(color_pc(:,2));
-color_pc(:,3) = im2double(color_pc(:,3));
-
-shapedimg = reshape(color_pc,[640,480,3]);
-
 filtered_image = reshape(rgb2hsv(color_pc),[640,480,3]);
-
 mask = find(filtered_image(:,:,2)<0.27);
-
-loc_pc(mask,:) = 0;
+mask2 = find(loc_pc(:,3)>1.5);
 color_pc(mask,:) = 0;
+color_pc(mask2,:) = 0;
+% disp(mask2)
+loc_pc(mask2,3) = 0;
 
 color_pc = reshape(color_pc, [640,480,3]);
 gray = rgb2gray(color_pc);
 
 mask_c = bwareaopen(gray, 1500);
 color_cont = bsxfun(@times, color_pc, cast(mask_c, 'like', color_pc));
+color_reshaped = reshape(color_cont,[307200,3]);
 %subplot(1,2,1), imshow(color_pc);
 %subplot(1,2,2), imshow(color_cont);
 
 
-%new = pointCloud(loc_pc, 'Color', color_pc);
+new = pointCloud(loc_pc, 'Color', color_reshaped);
 
-% pcshow(new)
+pcshow(new)
 
 %imshow(reshape(color_pc, [640,480,3]))
+end
